06462fd630dd26e7e846680926c8d1e642e00250
maxim <maxikavm@gmail.com>
2022-02-01 00:59:07 +0500

    make it better

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-#bot = telebot.TeleBot('5104763927:AAEc4bkq7nWqVKzeXOu-YF_Z5SjotTZn_7w')
-
 import telebot
-# Создаем экземпляр бота
+import random
+from telebot import types
+# Загружаем список интересных фактов
+f = open('data/facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
+# Загружаем список поговорок
+f = open('data/thinks.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
+# Создаем бота
 bot = telebot.TeleBot('5104763927:AAEc4bkq7nWqVKzeXOu-YF_Z5SjotTZn_7w')
-# Функция, обрабатывающая команду /start
+# Команда start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь cсволочь )')
+        # Добавляем две кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Факт")
+        item2=types.KeyboardButton("Поговорка")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+    # Если юзер прислал 1, выдаем ему случайный факт
+    if message.text.strip() == 'Факт' :
+            answer = random.choice(facts)
+    # Если юзер прислал 2, выдаем умную мысль
+    elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
