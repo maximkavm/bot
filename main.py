97c288f84e7b2fbfae295a46f7e6da3618645ca5
Maxim Meschankin <maxikavm@gmail.com>
2022-02-01 08:57:54 +0500

    1

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ f.close()
 f = open('data/thinks.txt', 'r', encoding='UTF-8')
 thinks  = f.read().split('\n')
 f.close()
+
+# Загружаем список поговорок
+f = open('data/telefon.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
+
 # Создаем бота
 bot = telebot.TeleBot('5104763927:AAEc4bkq7nWqVKzeXOu-YF_Z5SjotTZn_7w')
 # Команда start
@@ -18,8 +24,10 @@ def start(m, res=False):
         markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1=types.KeyboardButton("Факт")
         item2=types.KeyboardButton("Поговорка")
+        item3 = types.KeyboardButton("Телефоны")
         markup.add(item1)
         markup.add(item2)
+        markup.add(item3)
         bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
@@ -29,6 +37,8 @@ def handle_text(message):
             answer = random.choice(facts)
     # Если юзер прислал 2, выдаем умную мысль
     elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    elif message.text.strip() == 'Телефоны':
             answer = random.choice(thinks)
     # Отсылаем юзеру сообщение в его чат
     bot.send_message(message.chat.id, answer)
