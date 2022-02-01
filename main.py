b6a37c29971f2a8814a8a8671f0de1b9a8efbed6
Maxim Meschankin <maxikavm@gmail.com>
2022-02-01 16:48:47 +0500

    1

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import telebot
 import random
-from telebot import types
-from  db import BotDB
-BotDB = BotDB('accountant.db')
+
+from aiogram.dispatcher.filters import Command
+
+from db import BotDB
+import logging
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 
 # Загружаем список интересных фактов
 f = open('data/facts.txt', 'r', encoding='UTF-8')
@@ -13,50 +21,83 @@ f = open('data/thinks.txt', 'r', encoding='UTF-8')
 thinks = f.read().split('\n')
 f.close()
 
+
+def nt(str1):
+    answer = random.choice(str1)
+    return answer
+
+
 # Загружаем список поговорок
 f = open('data/telefon.txt', 'r', encoding='UTF-8')
 telf = f.read().split('\n')
 f.close()
 
-# Создаем бота
-bot = telebot.TeleBot('5104763927:AAEc4bkq7nWqVKzeXOu-YF_Z5SjotTZn_7w')
+button_hi = KeyboardButton('Привет! 👋')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
+
+BotDB = BotDB('accountant.db')
+
+# Объект бота
+bot = Bot(token="5104763927:AAEc4bkq7nWqVKzeXOu-YF_Z5SjotTZn_7w")
+
+# Диспетчер для бота
+dp = Dispatcher(bot)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+button_hi = KeyboardButton('Привет! 👋')
+button_tf = KeyboardButton('Телефонная книга')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
+greet_kb.add(button_tf)
 
 
-# Команда start
-@bot.message_handler(commands=["start"])
-async def start(m, res=False):
-    # Добавляем две кнопки
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton("Факт")
-        item2 = types.KeyboardButton("Поговорка")
-        item3 = types.KeyboardButton("Телефоны")
-        item4 = types.KeyboardButton("Телефонная книга")
-        markup.add(item1)
-        markup.add(item2)
-        markup.add(item3)
-        markup.add(item4)
-        await bot.send_message(m.chat.id,
-                         'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',
-                         reply_markup=markup)
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply(nt(facts), reply_markup=greet_kb)
 
 
-# Получение сообщений от юзера
-@bot.message_handler(content_types=["text"])
-async def handle_text(message):
-    # Если юзер прислал 1, выдаем ему случайный факт
-    global answer
-    if message.text.strip() == 'Факт':
-        answer = random.choice(facts)
-    # Если юзер прислал 2, выдаем умную мысль
-    elif message.text.strip() == 'Поговорка1':
-        answer = random.choice(thinks)
-    elif message.text.strip() == 'Телефоны':
-        answer = random.choice(telf)
-    elif message.text.strip() == 'Телефонная книга':
-        answer = BotDB.get_records(32)
-    # Отсылаем юзеру сообщение в его чат
-    await bot.send_message(message.chat.id, answer)
+@dp.message_handler(commands=['hi1'])
+async def process_hi1_command(message: types.Message):
+    await message.reply("Первое - изменяем размер клавиатуры", reply_markup=greet_kb)
 
+
+@dp.message_handler(commands=['hi2'])
+async def process_hi1_command(message: types.Message):
+    await message.reply(BotDB.get_records(32))
+
+
+# Хэндлер на команду /test1
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+if __name__ == "__main__":
+    # Запуск бота
+    executor.start_polling(dp, skip_updates=True)
 
 # Запускаем бота
-bot.polling(none_stop=True, interval=0)
+# bot.polling(none_stop=True, interval=0)
